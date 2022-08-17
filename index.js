9acd4d680d6082bc8b0107b755caa9764ba90dae
Yann Guillon <yann@dockdev.com>
2022-08-17 11:12:44 +0200

    readme

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ const qs = require('qs')
 
 const customParams = {
   url: ['url'],
-  endpoint: false
+  mode: ['mode'],
+  endpoint: false,
+  recipient: false,
+  walletID: false
 }
 
 const createRequest = (input, callback) => {
   const validator = new Validator(callback, input, customParams)
   const jobRunID = validator.validated.id
+  const mode = validator.validated.data.mode
+  const walletID = validator.validated.data.walletID
+  const recipient = validator.validated.data.recipient
   const loginURL = `${process.env.VUE_APP_VENLY_LOGIN_URL}/auth/realms/Arkane/protocol/openid-connect/token`
   const serviceURL = `${process.env.VUE_APP_VENLY_API_URL}/api/wallets`
   const walletType = 'WHITE_LABEL'
@@ -16,6 +22,11 @@ const createRequest = (input, callback) => {
   const identifier = 'socialclaim'
   const pincode = '1234'
   const url = validator.validated.data.url
+
+  console.log(mode)
+  console.log(mode)
+  console.log(walletID)
+  console.log(recipient)
 
   const params = {
     walletType,
@@ -37,14 +48,9 @@ const createRequest = (input, callback) => {
     timeout: 20000
   }
 
-
-
   Requester.request(login_config)
       .then(response => {
         const token = response.data.access_token
-        console.log("token: ")
-        console.log(token)
-
         const config = {
           method: 'post',
           headers: { 'content-type': 'application/json', 'Authorization': `Bearer ${token}` },
@@ -52,17 +58,22 @@ const createRequest = (input, callback) => {
           data:  params,
           timeout: 20000
         }
+        if (mode == 'getWallets') {
+          Requester.request(config)
+              .then(response => {
+                let id = null
+                if (response.data && response.data.result)
+                  id = response.data.result.id.replace(/-/g,'')
+                callback(response.status, Requester.success(jobRunID, {data: {result: id}}))
+              })
+              .catch(error => {
+                callback(200, Requester.success(jobRunID, {data: {result: null}}))
+              })
+        } else if (mode == 'executeTransfer') {
 
-        Requester.request(config)
-            .then(response => {
-              let id = null
-              if (response.data && response.data.result)
-                id = response.data.result.id.replace(/-/g,'')
-              callback(response.status, Requester.success(jobRunID, {data: {result: id}}))
-            })
-            .catch(error => {
-              callback(200, Requester.success(jobRunID, {data: {result: null}}))
-            })
+        } else {
+          callback(200)
+        }
       }).catch(error => {
         callback(200, Requester.success(jobRunID, {data: {result: null}}))
       })
